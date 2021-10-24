13f8b7e33db54743812a141ec74ad7e4aa738c9b
anita908 <anita248846@gmail.com>
2021-10-24 15:13:37 -0600

    moblie css styling add linkedin

--- src/App.js
+++ src/App.js
@@ -27,28 +27,33 @@ function App() {
        <p>Welcome to Anita's Personal Website</p>
         
         <h1 class="App-sub-headers">About me ! </h1>
-        <p>Go to BYU (go cougars!!)</p>
-        <p>Major in master of information system management (MISM)</p>
-        <p>Love swimming, helps me relax and exercise, must go every week</p>
-        <p>Enjoy programming, love how the code is connected</p>
+        <div class="App-description">
+          <p>Go to BYU (go cougars!!)</p>
+          <p>Major in master of information system management (MISM)</p>
+          <p>Love swimming, it helps me relax and exercise, must go every week !</p>
+          <p>Enjoy programming, love how the code is connected</p>
+          <p>Love BYU slogan/motto, ' Enter to Learn; Go Forth to Serve ', I hope to apply all I learn to serve others</p>
+        </div>
 
         <h1 class="App-sub-headers">Skills I have ! !</h1>
         <div class="App-skills">
           <span>React</span>
           <span>Redux</span>
           <span>JavaScript</span>
-          <span>wordpress</span>
+          <span>WordPress</span>
           <span>VBA</span>
           <span>ASP.NET</span>
-          <span>mySQL</span>
+          <span>MySQL</span>
           <span>C++</span>
-          <span>git</span>
-          <span>swift</span>
+          <span>Git</span>
+          <span>Swift</span>
           <span>JAVA</span>
           <span>HTML/CSS</span>
         </div>
 
         <h1 class="App-sub-headers">Projects</h1>
+
+        <h1 class="App-third-headers">Yvideo -</h1>
         <p class="App-yvideo-text">
           I currently work as a web developer at BYU. Our team is developing a web-based system for streaming video and audio content using React/Redux. 
           <a href={'http://yvideo.byu.edu/'} target="_blank" rel="noreferrer">http://yvideo.byu.edu/</a>
@@ -58,6 +63,7 @@ function App() {
         <h1 class="App-sub-headers">Contact Info</h1>
         <p>Email: anita248846@gmail.com</p>
         <p>Resume: <a href={resume} download>Click to download</a></p>
+        <p>LinkedIn: <a href={'https://www.linkedin.com/in/joyu-wu/'} target="_blank" rel="noreferrer">https://www.linkedin.com/in/joyu-wu/</a></p>
         <p>Github: <a href={'https://github.com/anita908'} target="_blank" rel="noreferrer">https://github.com/anita908</a></p>
         <div class="App-footer"></div>
       </header>
