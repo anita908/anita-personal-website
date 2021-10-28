95a7a8f1439de1a8ca0554fa74ccc82f2bb9dec7
anita908 <anita248846@gmail.com>
2021-10-28 01:12:06 -0600

    change tab icon add codewell project add highlight add copyright update resume

--- src/App.js
+++ src/App.js
@@ -22,21 +22,23 @@ function App() {
           <p>{currentTime.toLocaleDateString()} {currentTime.toLocaleTimeString()} </p>
           <h1>Anita Wu</h1>
         </div>
-      
-       <p>Welcome to Anita's Personal Website</p>
-        
-        <h1 class="App-sub-headers">About me !</h1>
-        <div class="App-description">
-          <p>My name is Jo-Yu, but I go with Anita</p>
-          <p>Go to BYU (go cougars!!)</p>
-          <p>Major in master of information system management (MISM)</p>
-          <p>Love swimming, it helps me relax and exercise, must go every week !</p>
-          <p>Enjoy programming, love how the code is connected</p>
-          <p>Love BYU slogan/motto, ' Enter to Learn; Go Forth to Serve ', I hope to apply all I learn to serve others</p>
+
+        <div>
+          <span className="App-sub-title">Welcome to Anita's Personal Website</span>
         </div>
 
-        <h1 class="App-sub-headers">Skills I have ! !</h1>
-        <div class="App-skills">
+        <h1 className="App-sub-headers">About me !</h1>
+        <div className="App-description">
+          <p>My name is Jo-Yu, but I go with <span>Anita</span></p>
+          <p>I Go to <span>BYU</span> (go cougars!!)</p>
+          <p>Major in master of information system management <span>(MISM)</span></p>
+          <p>Love swimming, it helps me relax and exercise, must go every week !</p>
+          <p>Enjoy programming, I love how the code is connected</p>
+          <p>Love BYU slogan/motto, 'Enter to Learn; Go Forth to Serve', I hope to apply all I learn to serve others</p>
+        </div>
+
+        <h1 className="App-sub-headers">Skills I have ! !</h1>
+        <div className="App-skills">
           <span>React</span>
           <span>Redux</span>
           <span>JavaScript</span>
@@ -51,21 +53,41 @@ function App() {
           <span>HTML/CSS</span>
         </div>
 
-        <h1 class="App-sub-headers">Projects</h1>
+        <h1 className="App-sub-headers">Projects ⟡</h1>
 
-        <h1 class="App-third-headers">Yvideo -</h1>
-        <p class="App-yvideo-text">
-          I currently work as a web developer at BYU. Our team is developing a web-based system for streaming video and audio content using React/Redux. 
+        <h1 className="App-third-headers">Yvideo -</h1>
+        <p className="App-project-text">
+          I currently work as a <span>front end developer at BYU</span>. Our team is developing a web-based system for streaming video and audio content using React/Redux. 
           <a href={'http://yvideo.byu.edu/'} target="_blank" rel="noreferrer">http://yvideo.byu.edu/</a>
         </p>
-        <a href={'https://github.com/BYU-ODH/yvideo-client'} target="_blank" rel="noreferrer"><img src={yvideo} alt={'yvideo'} class="App-yvideo-img"></img></a>
+        <a href={'https://github.com/BYU-ODH/yvideo-client'} target="_blank" rel="noreferrer"><img className="App-yvideo-img"  src={yvideo} alt={'yvideo'} ></img></a>
 
-        <h1 class="App-sub-headers">Contact Info</h1>
-        <p>Email: anita248846@gmail.com</p>
-        <p>Resume: <a href={resume} download>Click to download</a></p>
-        <p>LinkedIn: <a href={'https://www.linkedin.com/in/joyu-wu/'} target="_blank" rel="noreferrer">https://www.linkedin.com/in/joyu-wu/</a></p>
-        <p>Github: <a href={'https://github.com/anita908'} target="_blank" rel="noreferrer">https://github.com/anita908</a></p>
-        <div class="App-footer"></div>
+        <h1 className="App-third-headers">CodeWell -</h1>
+        <p className="App-project-text">
+          I had entrepreneurial experience to <span>start a company</span>. Our team had established an <span>educational platform</span> written by Typescript, aiming to teach programming via the Internet. 
+        </p>
+        <p className="App-project-text">
+          Following are our Github repositories and a short youtube video to show our students how to submit their assignments.
+        </p>
+        <div className="App-codewell-link">
+          <p>Github: </p>
+          <a href={'https://github.com/anita908/codewell-portal'} target="_blank" rel="noreferrer">Portal</a>
+          <a href={'https://github.com/anita908/codewell-server'} target="_blank" rel="noreferrer">Server</a>
+        </div>
+        <div className="App-codeWell-video">
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/J80DjuLNpBo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+
+        <h1 className="App-sub-headers">Contact Info</h1>
+        <div className="App-contact">
+          <p>Email: anita248846@gmail.com</p>
+          <p>Resume: <a href={resume} download>Click to download</a></p>
+          <p>LinkedIn: <a href={'https://www.linkedin.com/in/joyu-wu/'} target="_blank" rel="noreferrer">https://www.linkedin.com/in/joyu-wu/</a></p>
+          <p>Github: <a href={'https://github.com/anita908'} target="_blank" rel="noreferrer">https://github.com/anita908</a></p>
+        </div>
+        <div className="App-footer">
+          <footer> <small>&copy; Copyright {new Date().getFullYear()}, Jo-Yu Wu</small> </footer>
+        </div>
       </header>
     </div>
   );
